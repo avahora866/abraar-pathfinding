73f76b8f1458bc7b2f043ee3412722ff8e832dc5
Abraar Vahora <avahora1@hotmail.com>
2025-05-02 21:33:16 +0100

    Basic Pathing of Routes with Animation

--- path-ui/src/App.jsx
+++ path-ui/src/App.jsx
@@ -1,33 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const selectedRoute = [
+  [52.637603, -1.112897],
+  [52.636493, -1.111247],
+  [52.637124, -1.108847],
+  [52.637054, -1.108761],
+  [52.637058, -1.108474],
+  [52.636769, -1.108621],
+  [52.635824, -1.107943],
+  [52.635581, -1.107621],
+  [52.635497, -1.107401],
+  [52.635405, -1.106913],
+  [52.635413, -1.106714],
+  [52.635620, -1.105666],
+  [52.635664, -1.105204],
+  [52.635584, -1.104595],
+  [52.635166, -1.103375],
+  [52.632249, -1.105918],
+  [52.630973, -1.102372],
+  [52.630435, -1.099405],
+  [52.629872, -1.099513],
+  [52.628420, -1.100119]
+]
+
+const otherRoutes = [
+  [
+    [52.637603, -1.112897],
+    [52.636493, -1.111247],
+    [52.637124, -1.108847],
+    [52.637054, -1.108761],
+    [52.637058, -1.108474],
+    [52.636769, -1.108621],
+    [52.635824, -1.107943],
+    [52.635581, -1.107621],
+    [52.635497, -1.107401],
+    [52.635405, -1.106913],
+    [52.635413, -1.106714],
+    [52.635620, -1.105666],
+    [52.635664, -1.105204],
+    [52.635584, -1.104595],
+    [52.635166, -1.103375],
+    [52.632249, -1.105918],
+    [52.631060, -1.106781],
+    [52.629149, -1.107559],
+    [52.629123, -1.107280],
+    [52.628172, -1.104319],
+    [52.627378, -1.100779],
+    [52.628420, -1.100119]
+  ],
+  [
+    [52.637603, -1.112897],
+    [52.636493, -1.111247],
+    [52.637124, -1.108847],
+    [52.637054, -1.108761],
+    [52.637058, -1.108474],
+    [52.636769, -1.108621],
+    [52.635824, -1.107943],
+    [52.635581, -1.107621],
+    [52.635497, -1.107401],
+    [52.635405, -1.106913],
+    [52.635413, -1.106714],
+    [52.635620, -1.105666],
+    [52.635664, -1.105204],
+    [52.635584, -1.104595],
+    [52.635166, -1.103375],
+    [52.632249, -1.105918]
+
+  ],
+]
+
+const blueOptions = { color: '#166af2', weight: 5, smoothFactor: 4, noClip: false }
+const lightBlueOptions = { color: '#78abff', weight: 4, smoothFactor: 4 }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MapContainer center={[52.637603, -1.112897]} zoom={13} style={{ height: "100vh", width: "100%" }} zoomControl={false}>
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Polyline pathOptions={lightBlueOptions} positions={otherRoutes} className="animated-polyline" />
+        <Polyline pathOptions={blueOptions} positions={selectedRoute} className="animated-polyline"/>
+      </MapContainer>
     </>
   )
 }
