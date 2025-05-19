24776d85d4bf127a99f329ac48348f7217d9a93b
avahora866 <avahora1@hotmail.com>
2025-05-19 18:29:14 +0100

    Replacing original config project with spring web project

---
+++ path-engine/src/main/java/com/abraar/pathfinding/PathFindingApplication.java
@@ -0,0 +1,13 @@
+package com.abraar.pathfinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathFindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathFindingApplication.class, args);
+	}
+
+}
---
+++ path-engine/src/test/java/com/abraar/pathfinding/PathFindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraar.pathfinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathFindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
